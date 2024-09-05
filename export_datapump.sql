6ad8b8a505580058d91f6d28c607e3ec44c995d2
Jean - Samuel BOURGUIT <65498199+BOURGUITSamuel@users.noreply.github.com>
2024-09-05 22:21:40 +0200

    Update export_datapump.sql

--- export_datapump.sql
+++ export_datapump.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE DIRECTORY OPERIS_DATAPUMP AS '/opt/oracle/backup';
 
 -- Accorder les privilèges nécessaires
-grant read,write on directory OPERIS_DATAPUMP to system;
+grant read,write on directory PROD_DATAPUMP to system;
 grant datapump_exp_full_database to system;
 grant datapump_imp_full_database to system;
 
