784dcc298eac04267c2bf95ea8cd145ef6c4db6b
Jean - Samuel BOURGUIT <65498199+BOURGUITSamuel@users.noreply.github.com>
2024-11-12 22:30:02 +0100

    Update alter_default_profile.sql

--- alter_default_profile.sql
+++ alter_default_profile.sql
@@ -1,5 +1,5 @@
 -- Modifier les paramètres de sécurité du profil par défaut des utilisateurs
-alter profile default limit failed_login_attempts unlimited password_life_time unlimited;
+ALTER PROFILE DEFAULT LIMIT FAILED_LOGIN_ATTEMPTS UNLIMITED PASSWORD_LIFE_TIME UNLIMITED;
 
 -- Quitter la connexion
 exit;
