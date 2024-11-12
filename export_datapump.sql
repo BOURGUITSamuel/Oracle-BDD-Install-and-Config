bce9d4da2d91a40ed5618c40342a21c50c7af9a2
Jean - Samuel BOURGUIT <65498199+BOURGUITSamuel@users.noreply.github.com>
2024-11-12 22:30:54 +0100

    Update export_datapump.sql

--- export_datapump.sql
+++ export_datapump.sql
@@ -2,9 +2,9 @@
 CREATE OR REPLACE DIRECTORY PROD_DATAPUMP AS '/opt/oracle/backup';
 
 -- Accorder les privilèges nécessaires
-grant read,write on directory PROD_DATAPUMP to system;
-grant datapump_exp_full_database to system;
-grant datapump_imp_full_database to system;
+GRANT READ, WRITE ON DIRECTORY PROD_DATAPUMP TO SYSTEM;
+GRANT DATAPUMP_EXP_FULL_DATABASE TO SYSTEM;
+GRANT DATAPUMP_IMP_FULL_DATABASE TO SYSTEM;
 
 -- Quitter la connexion
 exit;
